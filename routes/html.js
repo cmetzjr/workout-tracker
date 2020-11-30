a50bd8e0fcdc20c3d22926ea0f9c3d3f8248c5aa
Charles Metzger <cmetzjr@yahoo.com>
2020-11-30 00:07:07 -0500

    fix html routes

--- routes/html.js
+++ routes/html.js
@@ -4,17 +4,17 @@ module.exports= function(app){
         
     //exercises
     app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname + "../public/exercise.html"));
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
     // stats
     app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname + "../public/stats.html"));
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 
     //index
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname + "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
 
