aa857831f75ca3f956c0b3ce9c2ed06e1bb90de6
Charles Metzger <cmetzjr@yahoo.com>
2020-11-29 23:06:09 -0500

    first commit

---
+++ routes/api.js
@@ -0,0 +1,41 @@
+const { Workout } = require("../models");
+var db = require("../models");
+const path = require ("path");
+
+module.exports = function(app) {
+
+    // get all workouts
+    app.get("/all", (req, res) => {
+        db.Workout.find({})
+          .then(data => {
+            res.json(data);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      });
+      
+    // get /workouts endpoint  
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+          .then(data => {
+            res.json(data);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      });
+
+    //get workouts in range from api/workouts/range  
+    
+
+    //post new workout (create)
+    
+
+    // put a workout (update)
+
+
+
+
+    
+}
---
+++ routes/html.js
@@ -0,0 +1,23 @@
+const path = require("path");
+module.exports= function(app){
+
+        
+    //exercises
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname + "../public/exercise.html"));
+    });
+
+    // stats
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname + "../public/stats.html"));
+    });
+
+    //index
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname + "../public/index.html"));
+    });
+
+
+    
+
+}
